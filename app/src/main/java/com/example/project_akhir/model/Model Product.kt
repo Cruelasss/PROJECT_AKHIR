18b36c3b5e67a01b1a7b60635e01e6364644299e
Cruelasss <irula5198@gmail.com>
2025-12-28 22:47:46 +0700

    Add User data model
    - Add `User` data class to `com.example.project_akhir.model`.
    - Include fields for `uid`, `email`, `full_name`, `phone_number`, and `is_verified`.

--- app/src/main/java/com/example/project_akhir/model/Model Product.kt
+++ app/src/main/java/com/example/project_akhir/model/Model Product.kt
@@ -1,2 +1,10 @@
 package com.example.project_akhir.model
 
+// Simpan di package com.example.project_akhir.model
+data class User(
+    val uid: String = "",          // Menggantikan user_id UUID
+    val email: String = "",        //
+    val full_name: String = "",    //
+    val phone_number: String = "", //
+    val is_verified: Boolean = false //
+)
