ab149456de9e316991fc8d4760df99efa5d36581
Cruelasss <irula5198@gmail.com>
2025-12-28 23:35:09 +0700

    Set up navigation and refactor MainActivity
    - Remove Firebase connection test code and log statements from `MainActivity`.
    - Replace placeholder greeting UI with `NavGraph` to manage application screens.
    - Clean up unused imports and boilerplate code in `MainActivity`.

--- app/src/main/java/com/example/project_akhir/MainActivity.kt
+++ app/src/main/java/com/example/project_akhir/MainActivity.kt
@@ -1,57 +1,20 @@
 package com.example.project_akhir
 
+
 import android.os.Bundle
-import android.util.Log // Tambahkan ini untuk melihat hasil di Logcat
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.example.project_akhir.ui.NavGraph
 import com.example.project_akhir.ui.theme.PROJECT_AKHIRTheme
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // --- KODE TES FIREBASE MULAI ---
-        val db = Firebase.firestore
-        val testData = hashMapOf(
-            "full_name" to "Tes Koneksi BekasinAja",
-            "status" to "Berhasil Tersambung!"
-        )
-
-        db.collection("connection_test")
-            .add(testData)
-            .addOnSuccessListener { documentReference ->
-                Log.d("FIREBASE_OK", "Data berhasil masuk! ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.e("FIREBASE_ERROR", "Gagal koneksi: ", e)
-            }
-        // --- KODE TES FIREBASE SELESAI ---
-
-        enableEdgeToEdge()
         setContent {
             PROJECT_AKHIRTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Firebase Test Running...",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // NavGraph sekarang mengontrol semua layar Anda
+                NavGraph()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Status: $name!", modifier = modifier)
 }
