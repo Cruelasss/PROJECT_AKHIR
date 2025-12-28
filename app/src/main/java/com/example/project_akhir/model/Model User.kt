a5653da5c5ab1d6cd2e0d37b2a29cf9f7cd6974c
Cruelasss <irula5198@gmail.com>
2025-12-28 22:48:12 +0700

    Add User data model
    - Define `User` data class with fields for `uid`, `email`, `full_name`, `phone_number`, and `is_verified`.

--- app/src/main/java/com/example/project_akhir/model/Model User.kt
+++ app/src/main/java/com/example/project_akhir/model/Model User.kt
@@ -1,2 +1,9 @@
 package com.example.project_akhir.model
 
+data class User(
+    val uid: String = "",          // Menggantikan user_id UUID
+    val email: String = "",        //
+    val full_name: String = "",    //
+    val phone_number: String = "", //
+    val is_verified: Boolean = false //
+)
