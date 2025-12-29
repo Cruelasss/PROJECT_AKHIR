a9c7a57ffb42341b2b86af35fa9d318be4b24eb1
Cruelasss <irula5198@gmail.com>
2025-12-29 11:37:32 +0700

    Add dependency for selectable modifier in AddProductScreen
    - Import `androidx.compose.foundation.selection.selectable` in `AddProductScreen.kt`.

--- app/src/main/java/com/example/project_akhir/ui/screens/AddProductScreen.kt
+++ app/src/main/java/com/example/project_akhir/ui/screens/AddProductScreen.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
