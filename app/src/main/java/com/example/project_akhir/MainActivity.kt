dcc4565027bf3d4471c875cf48fce73ff0e165da
Cruelasss <irula5198@gmail.com>
2025-12-29 17:10:57 +0700

    Update entry point to use MainScreen in MainActivity
    - Replace `NavGraph()` with `MainScreen()` in the setContent block.
    - Update imports to reflect the change from `ui.NavGraph` to `ui.screens.MainScreen`.

--- app/src/main/java/com/example/project_akhir/MainActivity.kt
+++ app/src/main/java/com/example/project_akhir/MainActivity.kt
@@ -4,7 +4,8 @@ package com.example.project_akhir
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.project_akhir.ui.NavGraph
+
+import com.example.project_akhir.ui.screens.MainScreen
 import com.example.project_akhir.ui.theme.PROJECT_AKHIRTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PROJECT_AKHIRTheme {
                 // NavGraph sekarang mengontrol semua layar Anda
-                NavGraph()
+                MainScreen()
             }
         }
     }
